59b0e3b1bdaa9b9caf1e57a551b2d4b52042a01a
DESKTOP-1C6TJM2\giarg <giargo16@gmail.com>
2018-08-19 21:50:21 +0200

    Seconda lezione del tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ListComponent } from './components/list/list.component';
 import { CreateComponent } from './components/create/create.component';
 import { EditComponent } from './components/edit/edit.component';
+import { MatToolbarModule } from '@angular/material';
+
+import { IssueService} from './issue.service';
 
 const routes: Routes = [
   { path: 'create', component: CreateComponent },
@@ -25,9 +29,11 @@ const routes: Routes = [
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    RouterModule.forRoot(routes)
+    HttpClientModule,
+    RouterModule.forRoot(routes),
+    MatToolbarModule
   ],
-  providers: [],
+  providers: [IssueService],
   bootstrap: [AppComponent]
 })
 
